b8465ac94de81fb89600be91d16dc44fd0a63aa9
Lorenzo Fabbri <LORENZO.FABBRI.4G@local.ittcesena.it>
2020-11-03 18:00:58 +0100

    Aggiunta classe brano

---
+++ Es git/Brano.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_git
+{
+    class Brano
+    {
+        public Brano(string titolo, string autore, double durata)
+        {
+            Titolo = titolo;
+            Autore = autore;
+            Durata = durata;
+        }
+
+        public string Titolo
+        {
+            get;
+            set;
+        }
+
+        public string Autore
+        {
+            get;
+            set;
+        }
+
+        public double Durata
+        {
+            get;
+            set;
+        }
+
+        public bool shortSong(double durata)
+        {
+            if (Durata < durata)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return Titolo + " " + Autore + " " + Durata;
+        }
+    }
+}
