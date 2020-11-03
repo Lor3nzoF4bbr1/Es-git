07e3e68b2f7d9cbf8dfea8634cfca25d8a867d05
MattiaFilippi <mattia.filippi.stud@ispascalcomandini.it>
2020-11-03 19:44:10 +0100

    Step 2: aggiunti i metodi di lettura da file e scrittura su listbox

--- Es git/MainWindow.xaml.cs
+++ Es git/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Es_git
 {
@@ -20,9 +21,54 @@ namespace Es_git
     /// </summary>
     public partial class MainWindow : Window
     {
+        CD cd;
         public MainWindow()
         {
             InitializeComponent();
+
+            LeggiFile();
+
+            scriviInListbox();
+        }
+
+        public void LeggiFile()
+        {
+            List<Brano> brani = new List<Brano>();
+            string[] elementiCD;
+            string fileBrani = "";
+
+            using (StreamReader file = new StreamReader("CD.txt"))
+            {
+                string linea = file.ReadLine();
+
+                elementiCD = linea.Split('|');
+                fileBrani = elementiCD[2];
+            }
+
+            using (StreamReader file = new StreamReader(fileBrani))
+            {
+                while (!file.EndOfStream)
+                {
+                    string linea = file.ReadLine();
+
+                    string[] elementi = linea.Split('|');
+
+                    Brano b = new Brano(elementi[0], elementi[1], double.Parse(elementi[2]));
+                    brani.Add(b);
+                }
+            }
+
+            cd = new CD(brani, elementiCD[0], elementiCD[1]);
+        }
+
+        private void scriviInListbox()
+        {
+            lstBrani.Items.Clear();
+
+            foreach (Brano b in cd.ListaBraniInCD)
+            {
+                lstBrani.Items.Add(b.ToString());
+            }
         }
     }
 }
